181a3d878e188e0dcec6353fe7b8c9134bcfc39b
Kabir <sagithedeveloper@gmail.com>
2026-03-18 08:56:06 +0530

    added

---
+++ 04_React JS/S06_State/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Home from "./components/home/Home.jsx";
+import Header from "./components/header/Header.jsx";
+import Name from "./components/state/Name.jsx";
+function App() {
+  return (
+    <div>
+      <Name />
+      {/* <Header />
+      <Home /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ 04_React JS/S07/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Map from "./components/map/Map.jsx";
+
+import Profile from "./components/profile/Profile.jsx";
+function App() {
+  return (
+    <div>
+      <Map />
+      {/* <Profile /> */}
+    </div>
+  );
+}
+
+export default App;
